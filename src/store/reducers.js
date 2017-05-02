f1cf9c4cecb661d530cbe6844aba57db500f630c
Erika Sankari <8eikochan@gmail.com>
2017-05-02 15:57:08 +0300

    Add shopping cart, login and logout constants and reducers.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,65 +2,45 @@ import C from '../constants';
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
-// Reducer functions here
-export const product = (state = null, action) =>
-    (action.type === C.ADD_PRODUCT) ? action.payload : state;
-
-export const allProducts = (state = [], action) => {
+export const shoppingCart = (state = [], action) => {
     switch (action.type) {
-        case C.ADD_PRODUCT:
+        case C.ADD_TO_CART:
             return [
-                ...state,
-                product(null, action)
-            ];
+                    ...state,
+                    action.payload
+                ];
 
-        case C.BUY_PRODUCT:
-            // Filters out product that has action.payload as ID
+        case C.REMOVE_FROM_CART:
             return state.filter(product => product.id !== action.payload);
+
+        case C.CLEAR_CART:
+            return [];
 
         default:
             return state;
     }
 };
 
-export const fetching = (state = false, action) => {
+export const authentication = (state = {loggedIn:false}, action) => {
+    switch (action.type) {
+        case C.LOGIN:
+            return {
+                loggedIn: true,
+                user: action.payload
+            };
 
-    switch(action.type) {
-
-        case C.FETCH_PRODUCT_NAMES :
-            return true;
-
-        case C.CANCEL_FETCHING :
-            return false;
-
-        case C.CHANGE_SUGGESTIONS :
-            return false;
+        case C.LOGOUT:
+            return {
+                loggedIn: false
+            };
 
         default:
-            return state
-    }
-};
-
-export const suggestions = (state = [], action) => {
-
-    switch(action.type) {
-
-        case C.CLEAR_SUGGESTIONS :
-            return [];
-
-        case C.CHANGE_SUGGESTIONS :
-            return action.payload;
-
-        default :
-            return state
+            return state;
     }
 };
 
 export default combineReducers({
-    allProducts,
-    productNames: combineReducers({
-        fetching,
-        suggestions
-    }),
+    shoppingCart,
+    authentication,
     routing
 })
