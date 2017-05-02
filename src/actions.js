6c23a7fc95e0094f5432a8892fa000e0a61a460a
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-05-02 16:51:54 +0300

    Create action creators for login, logout and shopping cart actions.

--- src/actions.js
+++ src/actions.js
@@ -1,55 +1,24 @@
 import C from './constants';
-import fetch from 'isomorphic-fetch';
 
-
-
-export const addProduct = (id, name) => ({
-    type: C.ADD_PRODUCT,
-    payload: {
-        id,
-        name
-    }
+export const login = user => ({
+    type: C.LOGIN,
+    payload: user
 });
 
-export const buyProduct = (id) => ({
-    type: C.BUY_PRODUCT,
+export const logout = () => ({
+    type: C.LOGOUT
+});
+
+export const addToCart = product => ({
+    type: C.ADD_TO_CART,
+    payload: product
+});
+
+export const removeFromCart = id => ({
+    type: C.REMOVE_FROM_CART,
     payload: id
 });
 
-export const suggestProductNames = value => dispatch => {
-
-    dispatch({
-        type: C.FETCH_PRODUCT_NAMES
-    });
-
-    fetch('http://localhost:8080/api/products/search/contains?name=' + value)
-        .then(response => response.json())
-        .then(json => {
-            let products = json._embedded.products;
-
-            if (products && products.length > 0) {
-                dispatch({
-                    type: C.CHANGE_SUGGESTIONS,
-                    payload: json._embedded.products
-                });
-            } else {
-                dispatch({
-                    type: C.CANCEL_FETCHING
-                });
-
-                dispatch(clearSuggestions());
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            dispatch({
-                type: C.CANCEL_FETCHING
-            });
-
-            dispatch(clearSuggestions());
-        })
-};
-
-export const clearSuggestions = () => ({
-    type: C.CLEAR_SUGGESTIONS
+export const clearCart = () => ({
+    type: C.CLEAR_CART
 });
