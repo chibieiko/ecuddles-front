47ee446d67c118cef747ba827d05fa7dc6d51d7b
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-05-07 23:21:51 +0300

    Implement basic version of product page.

---
+++ src/components/ui/Stars.js
@@ -0,0 +1,34 @@
+/**
+ * Created by vili on 07/05/2017.
+ */
+
+import {Component} from 'react';
+import '../../stylesheets/stars.scss';
+
+export default class Stars extends Component {
+    constructor(props) {
+        super(props);
+
+        let stars = [];
+
+        for (let i = 0; i < 5; i++) {
+            let addClass = i < Math.round(this.props.rating) ? "star-filled" : "star-empty";
+
+            stars.push(
+                <span key={i} className={"star " + addClass + " glyphicon glyphicon-star"}/>
+            );
+        }
+
+        this.state = {
+            stars: stars
+        };
+    };
+
+    render() {
+        return <span className="star-container">
+            {
+                this.state.stars
+            }
+        </span>;
+    };
+};
