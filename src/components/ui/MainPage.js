54a8717f9f36a3e411f3e25221a1cc714252340a
Erika Sankari <8eikochan@gmail.com>
2017-05-16 09:19:08 +0300

    Enable pagination to product list.

--- src/components/ui/MainPage.js
+++ src/components/ui/MainPage.js
@@ -1,14 +1,17 @@
 import {Component} from 'react';
 import ProductList from './ProductList';
 
-export default class LoginPage extends Component {
+export default class MainPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            search: ""
+        };
     };
 
     render() {
         return <div>
-            <ProductList search=""/>
+            <ProductList/>
         </div>;
     };
 };
--- src/components/ui/ProductList.js
+++ src/components/ui/ProductList.js
@@ -9,7 +9,9 @@ export default class ProductList extends Component {
         super(props);
 
         this.state = {
-            page: 0,
+            page: {
+                number: 0
+            },
             fetching: false,
             products: []
         };
@@ -31,21 +33,48 @@ export default class ProductList extends Component {
             fetching: true
         });
 
-        let url = "";
+        let url;
 
-        if (this.props.category) {
-            url = "/categories/" + this.props.category;
+        if (this.props.search) {
+            console.log("SEARCHING");
+            this.setState({
+                page: {
+                    number: 0
+                }
+            }, () => {
+                url = "/products/search/contains/?name=" + this.props.search +
+                    "&page=" + this.state.page.number;
+                this.getProducts(url);
+            });
+
+        } else if (this.props.category) {
+            console.log("I HAVE A CATEGORY");
+            this.setState({
+                page: {
+                    number: 0
+                }
+            }, () => {
+                url = "/products/search/has?categoryid=" + this.props.category + "&page=" + this.state.page.number;
+                this.getProducts(url);
+            });
         } else {
-            url = "/products/search/contains/?name=" + this.props.search + "&page=" + this.state.page;
+            console.log("default url");
+            url = "/products/search/has?categoryid=1&page=" + this.state.page.number;
+            this.getProducts(url);
         }
+    };
 
+    getProducts = (url) => {
+        console.log("getting products with url: ", url);
         connector(url)
             .then(response => {
                 let products = response._embedded.products;
+                let page = response.page;
 
                 this.setState({
                     fetching: false,
-                    products: products ? products : []
+                    products: products ? products : [],
+                    page: page
                 });
             })
             .catch(() => {
@@ -55,14 +84,62 @@ export default class ProductList extends Component {
             });
     };
 
+    nextPage = () => {
+        if (this.state.page.number < this.state.page.totalPages - 1) {
+            let pageNum = this.state.page.number + 1;
+            this.setState({
+                page: {
+                    number: pageNum
+                }
+            }, () => {
+                console.log("page number next:::", this.state.page.number);
+                this.loadProducts();
+            });
+        }
+    };
+
+    previousPage = () => {
+        if (this.state.page.number > 0) {
+            let pageNum = this.state.page.number - 1;
+            this.setState({
+                page: {
+                    number: pageNum
+                }
+            }, this.loadProducts);
+        }
+    };
+
     render() {
         return <div>
             <h1>Latest additions to our cuddly family</h1>
             {
                 this.state.fetching ?
                     <Spinner margin={true}/> :
-                    this.state.products.map(product => <ProductCard key={product.id} product={product}/>)
+                    this.state.products.map(product => <ProductCard
+                        key={product.id} product={product}/>)
             }
+            <div className="col-xs-12">
+                <nav aria-label="Product page navigation">
+                    <ul className="pager">
+                        <li className="previous">
+                            <a onClick={this.previousPage}>
+                                <span className="hidden-xs">Previous page</span>
+                                <span className="glyphicon glyphicon-arrow-left hidden-lg hidden-md hidden-sm col-xs-1"/>
+                            </a>
+                        </li>
+
+                        <li className="next">
+                            <a onClick={this.nextPage}>
+                                <span className="hidden-xs">Next page</span>
+                                <span className="glyphicon glyphicon-arrow-right hidden-lg hidden-md hidden-sm col-xs-1"/>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <p>total pages: {this.state.page.totalPages}</p>
+                <p>total elements: {this.state.page.totalElements}</p>
+                <p>current page: {this.state.page.number}</p>
+            </div>
         </div>;
     };
 };
